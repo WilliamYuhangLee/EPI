4038d3eb0723a90f60e3f8e109dd041703fcc91a
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2019-05-13 01:46:04 -0400

    Complete SmallestNonconstructableValue (page 31/32)

--- epi_judge_java/epi/SmallestNonconstructibleValue.java
+++ epi_judge_java/epi/SmallestNonconstructibleValue.java
@@ -1,20 +1,34 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.Collections;
 import java.util.List;
+
 public class SmallestNonconstructibleValue {
-  @EpiTest(testDataFile = "smallest_nonconstructible_value.tsv")
+    @EpiTest(testDataFile = "smallest_nonconstructible_value.tsv")
 
-  public static int smallestNonconstructibleValue(List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    public static int smallestNonconstructibleValue(List<Integer> A) {
+        // TODO - you fill in here.
+        Collections.sort(A);
+        int SNV = 1;
+        for (Integer integer : A) {
+            if (integer > SNV) {
+                return SNV;
+            } else {
+                SNV += integer;
+            }
+        }
+        return SNV;
+    }
 
-  public static void main(String[] args) {
-    System.exit(
+    public static void main(String[] args) {
+        System.exit(
         GenericTest
-            .runFromAnnotations(args, "SmallestNonconstructibleValue.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        .runFromAnnotations(args, "SmallestNonconstructibleValue.java",
+                            new Object() {
+                            }.getClass().getEnclosingClass())
+        .ordinal());
+    }
 }
